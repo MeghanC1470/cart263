14e0234b6ad96c0430a24dbb3b9c2bf7d9ff49ac
Meghan <MeghanC2000@outlook.com>
2020-01-07 17:09:06 -0500

    Act 1: Setting Players and Food

--- p5-project/js/script.js
+++ p5-project/js/script.js
@@ -2,20 +2,38 @@
 
 /*****************
 
-Title of Project
-Author Name
+Activity 1: Circle Eater
+Meghan Cullen
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
+// Player
+let player = {
+  x: 0,
+  y: 0,
+  maxSize: 50,
+  size: 50,
+  active: true,
+  color: '#cccc55'
+}
+
+let food = {
+  x: 0,
+  y: 0,
+  size: 100,
+  color: '#cccc60'
+}
 
 // preload()
 //
 // Description of preload
 
 function preload() {
-
+createCanvas(windowWidth,windowHeight);
+foodPosition();
+noCursor();
 }
 
 
