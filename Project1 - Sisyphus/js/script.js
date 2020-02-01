0ad9a3bf0c50e944d20c2c7f12758dfad3d4e7aa
Meghan <MeghanC2000@outlook.com>
2020-02-01 14:16:22 -0500

    P1: Beginning

---
+++ Project1 - Sisyphus/js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/*****************
+
+Title
+Meghan Cullen
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+
+******************/
+
+
+$(document).ready(setup);
+
+function setup() {
+
+}
