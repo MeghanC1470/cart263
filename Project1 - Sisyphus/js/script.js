2193f0724db4bf43ffe0bc51834feab031032faa
Meghan <MeghanC2000@outlook.com>
2020-02-04 14:15:14 -0500

    P1: Removable Bear Enabled

--- Project1 - Sisyphus/js/script.js
+++ Project1 - Sisyphus/js/script.js
@@ -12,20 +12,33 @@ author, and this description to match your project!
 
 let $box
 let $ball
+let $bear
+let $train
 
 $(document).ready(setup);
 
 function setup() {
   $box = $('#box');
   $box.droppable({
-  drop: onDrop
+    drop: onDrop
 });
   $ball = $('#ball');
   $ball.draggable({
-  revert: "valid"
+    revert: "valid"
 });
+  $bear = $('.bear');
+  $bear.draggable();
+  $train = $('#train');
+  $train.draggable({
+    revert: "valid"
+  })
 }
 
-function onDrop(){
-$ball.draggable()
+function onDrop(event, ui){
+$ball.draggable();
+$train.draggable();
+if ( $(ui.draggable).hasClass("bear") === true){
+$(ui.draggable).remove();
+}
+  console.log("bear");
 }
