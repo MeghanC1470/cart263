ec80554b2708022e1566f2127855a48edf408cb8
Meghan <MeghanC2000@outlook.com>
2020-02-04 21:33:06 -0500

    P1: Music Enabled

--- Project1 - Sisyphus/js/script.js
+++ Project1 - Sisyphus/js/script.js
@@ -18,8 +18,11 @@ let $car;
 let $horse;
 let $duck;
 
+let backgroundMusic = new Audio("assets/sounds/Music.mp3")
+let packingSound = new Audio("assets/sounds/PackingSound.mp3")
+
 let random;
-let message = ['Put your toys away Lil Sisyphus', 'Why have you not put your toys away yet?', 'Put your toys away right now!', 'Are you happy, Sisyphus?', 'Are you almost done Sisyphus?', 'I am going to count to 3 and the toys better be away!', 'Clean up your mess, Sisyphus!', 'All done?', 'What are you doing Sisyphus?'];
+let message = ['Put your toys away Lil Sisyphus', 'Why have you not put your toys away yet?', 'Put your toys away right now!', 'Are you happy, Sisyphus?', 'Are you almost done Sisyphus?', 'I am going to count to 3 and the toys better be away!', 'Clean up your mess, Sisyphus!', 'All done?', 'What are you doing Sisyphus?', 'I thought I said I wanted your toys away!', 'Do not lie to me, Sisyphus!'];
 
 $(document).ready(setup);
 
@@ -46,6 +49,8 @@ function setup() {
   $car.draggable();
   $horse = $('.horse');
   $horse.draggable();
+
+  backgroundMusic.play();
 }
 
 function onDrop(event, ui){
@@ -56,6 +61,7 @@ $train.draggable();
 $duck.draggable();
 if ( $(ui.draggable).hasClass("bear") === true){
 $(ui.draggable).remove();
+packingSound.play();
   }
 if ( $(ui.draggable).hasClass("car") === true){
 $(ui.draggable).remove();
@@ -63,4 +69,5 @@ $(ui.draggable).remove();
 if ( $(ui.draggable).hasClass("horse") === true){
   $(ui.draggable).remove();
     }
+packingSound.play();
 }
