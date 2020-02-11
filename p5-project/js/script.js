b0f1e956332258ec9d49cac753f663958cc4ac40
Meghan <MeghanC2000@outlook.com>
2020-02-11 17:09:19 -0500

    Class 5

--- p5-project/js/script.js
+++ p5-project/js/script.js
@@ -9,43 +9,184 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
-let $mouth;
-let $fly;
-let buzzSound = new Audio("assets/sounds/buzz.mp3");
-let mouthSound = new Audio("assets/sounds/crunch.wav")
+let animals = [
+      "aardvark",
+      "alligator",
+      "alpaca",
+      "antelope",
+      "ape",
+      "armadillo",
+      "baboon",
+      "badger",
+      "bat",
+      "bear",
+      "beaver",
+      "bison",
+      "boar",
+      "buffalo",
+      "bull",
+      "camel",
+      "canary",
+      "capybara",
+      "cat",
+      "chameleon",
+      "cheetah",
+      "chimpanzee",
+      "chinchilla",
+      "chipmunk",
+      "cougar",
+      "cow",
+      "coyote",
+      "crocodile",
+      "crow",
+      "deer",
+      "dingo",
+      "dog",
+      "donkey",
+      "dromedary",
+      "elephant",
+      "elk",
+      "ewe",
+      "ferret",
+      "finch",
+      "fish",
+      "fox",
+      "frog",
+      "gazelle",
+      "gila monster",
+      "giraffe",
+      "gnu",
+      "goat",
+      "gopher",
+      "gorilla",
+      "grizzly bear",
+      "ground hog",
+      "guinea pig",
+      "hamster",
+      "hedgehog",
+      "hippopotamus",
+      "hog",
+      "horse",
+      "hyena",
+      "ibex",
+      "iguana",
+      "impala",
+      "jackal",
+      "jaguar",
+      "kangaroo",
+      "koala",
+      "lamb",
+      "lemur",
+      "leopard",
+      "lion",
+      "lizard",
+      "llama",
+      "lynx",
+      "mandrill",
+      "marmoset",
+      "mink",
+      "mole",
+      "mongoose",
+      "monkey",
+      "moose",
+      "mountain goat",
+      "mouse",
+      "mule",
+      "muskrat",
+      "mustang",
+      "mynah bird",
+      "newt",
+      "ocelot",
+      "opossum",
+      "orangutan",
+      "oryx",
+      "otter",
+      "ox",
+      "panda",
+      "panther",
+      "parakeet",
+      "parrot",
+      "pig",
+      "platypus",
+      "polar bear",
+      "porcupine",
+      "porpoise",
+      "prairie dog",
+      "puma",
+      "rabbit",
+      "raccoon",
+      "ram",
+      "rat",
+      "reindeer",
+      "reptile",
+      "rhinoceros",
+      "salamander",
+      "seal",
+      "sheep",
+      "shrew",
+      "silver fox",
+      "skunk",
+      "sloth",
+      "snake",
+      "squirrel",
+      "tapir",
+      "tiger",
+      "toad",
+      "turtle",
+      "walrus",
+      "warthog",
+      "weasel",
+      "whale",
+      "wildcat",
+      "wolf",
+      "wolverine",
+      "wombat",
+      "woodchuck",
+      "yak",
+      "zebra"
+    ]
+  let correctAnimal
+  let answers = [];
+  const NUM_OPTIONS = 6;
 
 $(document).ready(setup);
 
 function setup() {
-  $mouth = $('#mouth');
-  $mouth.droppable({
-  drop: onDrop
-});
-  $fly = $('#fly');
-  $fly.draggable();
-  console.log('fly');
-  buzzSound.loop = true;
-  $fly.on('mousedown', function() {
-    buzzSound.play();
-  });
-  $fly.on('mouseup', function() {
-    buzzSound.pause();
-  });
+ newRound();
 }
 
-function onDrop(){
-$fly.remove()
-$(this).attr('src','assets/images/mouth2.png');
-setInterval(chew, 150);
+function addButton(label){
+  let $div = $('<div></div>');
+  $div.addClass('guess');
+  $div.text(label);
+  $div.button();
+  $div.appendTo('body');
+  $div.on('click',handleGuess);
 }
 
-function chew(){
-  if ($mouth.attr('src') === 'assets/images/mouth.png') {
-    $mouth.attr('src','assets/images/mouth2.png');
-  mouthSound.currentTime = 0;
-  mouthSound.play();
+function newRound(){
+  answers = [];
+  for (let i = 0; i < NUM_OPTIONS; i++) {
+    let answer = animals[Math.floor(Math.random()* animals.length)];
+    addButton(answer);
+    answers.push(answer);
+  }
+    correctAnimal = answers[Math.floor(Math.random()* answers.length)];
+   sayBackwards(correctAnimal);
 }
-else {
-  $mouth.attr('src','assets/images/mouth.png');
-}
-}
+  function handleGuess(){
+    if ($(this).text() === correctAnimal) {
+    $('.guess').remove();
+    setTimeout(newRound,100);
+    }
+    else ()
+  }
+
+  function sayBackwards(text){
+    let backwardsText = text.split('').reverse().join('');
+    let option = {
+      rate: Math.random(),
+      pitch: Math.random()
+    };
+    responsiveVoice.speak(backwardsText);
+  }
