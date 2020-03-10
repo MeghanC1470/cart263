48050157b34f1566db3e8957cf9ab43436147a42
Meghan <MeghanC2000@outlook.com>
2020-03-10 16:25:07 -0400

    JSON2

--- jquery-project/js/script.js
+++ jquery-project/js/script.js
@@ -14,5 +14,37 @@ $(document).ready(setup);
 
 
 function setup() {
-$.getJSON("data/Data1.json");
+$.getJSON("data/data2.json")
+.done(dataLoaded)
+.fail(dataNotLoaded);
+}
+
+function dataLoaded(data){
+console.log(data);
+
+let randomCondiment = getRandomArrayElement(data.condiments);
+console.log(randomCondiment)
+let verb = "is";
+if (randomCondiment.charAt(randomCondiment.length - 1) === "s") {
+  verb = "are";
+  }
+console.log(verb);
+let randomCat = getRandomArrayElement(data.cats);
+console.log(randomCat);
+let randomRoom = getRandomArrayElement(data.rooms);
+console.log(randomRoom);
+
+let randomDescription = `${randomCondiment} ${verb} like a ${randomCat} in a ${randomRoom}`
+$('body').append(randomDescription);
+}
+
+
+
+function dataNotLoaded(request, text, error){
+  console.error(error);
+}
+
+function getRandomArrayElement(array){
+  let element = array[Math.floor(Math.random() * array.length)];
+  return element;
 }
