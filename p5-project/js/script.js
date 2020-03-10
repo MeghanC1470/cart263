c3dafd67ddea7b03954fe4bac10e3de332ce61bc
Meghan <MeghanC2000@outlook.com>
2020-03-10 15:43:27 -0400

    JSON Project

--- p5-project/js/script.js
+++ p5-project/js/script.js
@@ -9,58 +9,14 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
-const NOTE_TEMPO = 500;
-const DRUM_TEMPO = 250;
-
-let frequencies = [
-  110.00,
-  123.47,
-  69.30,
-  36.71,
-  82.41,
-  92.50,
-  103.83
-];
-
-let synth = new Pizzicato.Sound({
-  source: 'wave',
-});
-
-var kick = new Pizzicato.Sound('assets/sounds/kick.wav');
-var snare = new Pizzicato.Sound('assets/sounds/snare.wav');
-var hihat = new Pizzicato.Sound('assets/sounds/hihat.wav');
-
-let beat = 0;
-let pattern = ['x', '*', 'xo*', ' ', 'x', 'x', 'xo', '*'];
-
-
-// setup
-//
-//
-function setup(){
-
+let tarot;
+function setup() {}
+function mousePressed() {
+  loadJSON("data/tarot_interpretations.json", tarotLoaded);
 }
-
-function mousePressed(){
- setInterval(playNote, NOTE_TEMPO);
- setInterval(playDrum, DRUM_TEMPO)
-}
-
-function playNote(){
-  let note = frequencies[Math.floor(Math.random()* frequencies.length)];
-  synth.frequency = note;
-  synth.play();
-}
-
-function playDrum(){
-  let symbols = pattern[beat];
-  if (symbols.includes('x')) {
-    kick.play();
-  }
-  if (symbols.includes('xo')) {
-    snare.play();
-  }
-  if (symbols.includes('*')) {
-    hihat.play();
+function tarotLoaded(data) {
+  tarot = data.tarot_interpretations; // We only want the actual cards
+  for (let i = 0; i < tarot.length; i++) {
+    console.log(tarot[i].name);
   }
 }
