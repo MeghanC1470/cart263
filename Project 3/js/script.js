e015e8da71ba5209e810d176731665526874bc61
Meghan <MeghanC2000@outlook.com>
2020-03-24 14:32:33 -0400

    P3: Enabled Test Pizzicato and Annyang

--- Project 3/js/script.js
+++ Project 3/js/script.js
@@ -2,17 +2,145 @@
 
 /*****************
 
-Title
+Project 3
 Meghan Cullen
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
+// Time for one note
+const NOTE_TEMPO = 500;
+// Time for one beat
+const DRUM_TEMPO = 250;
+// Attack time for a note (in seconds)
+const ATTACK = 0.1;
+// Release time for a note (in seconds)
+const RELEASE = 0.1;
 
+// We need an array of the possible notes to play as frequencies (in Hz)
+// A Major =  A, B, C♯, D, E, F♯, and G♯
+// We can get the frequencies of these notes from THE INTERNET, e.g.
+// http://pages.mtu.edu/~suits/notefreqs.html
+let frequencies = [
+  220, 246.94, 277.18, 293.66, 329.63, 369.99, 415.30
+];
+// The synth
+let synth;
+// The sound files
+let kick;
+let snare;
+let hihat;
+// Our drum pattern
+// Each array element is one beat and has a string with each
+// drum to play for that beat
+// x = kick, o = snare, * = hihat
+let pattern = ['x', '*', 'xo*', ' ', 'x', 'x', 'xo', '*'];
+// Which beat of the pattern we're at right now
+let beat = 0;
+
+if (annyang) {
+//Let's define a command.
+var commands = {
+  'Hello': function(){
+    console.log("hello");
+  }
+};
+
+//Add our commands to annyang
+annyang.addCommands(commands);
+
+//Start listening.
+annyang.start();
+};
 
 $(document).ready(setup);
 
 function setup() {
 
+  // Create the synth
+  synth = new Pizzicato.Sound({
+    source: 'wave',
+    options: {
+      type: 'sine',
+      frequency: 220,
+      attack: ATTACK,
+      release: RELEASE
+    }
+  });
+
+  // Load the three drum sounds as wav files
+  kick = new Pizzicato.Sound({
+    source: 'file',
+    options: {
+      path: 'assets/sounds/kick.wav'
+    }
+  });
+
+  snare = new Pizzicato.Sound({
+    source: 'file',
+    options: {
+      path: 'assets/sounds/snare.wav'
+    }
+  });
+
+  hihat = new Pizzicato.Sound({
+    source: 'file',
+    options: {
+      path: 'assets/sounds/hihat.wav'
+    }
+  });
 }
+
+// mousePressed
+//
+// Using this to start the note and drum sequences to get around
+// user interaction (and to give the files time to load)
+function mousePressed() {
+  // Start an interval for the notes
+  setInterval(playNote, NOTE_TEMPO);
+  // Start an interval for the drums
+  setInterval(playDrum, DRUM_TEMPO);
+}
+
+// playNote
+//
+// Chooses a random frequency and assigns it to the synth
+function playNote() {
+  // Pick a random frequency from the array
+  let frequency = frequencies[Math.floor(Math.random() * frequencies.length)];
+  // Set the synth's frequency
+  synth.frequency = frequency;
+  // If it's not already playing, play the synth
+  synth.play();
+}
+
+// playDrum()
+//
+// Checks the string representing the drums for the current beat
+// and plays the appropriate sounds
+function playDrum() {
+  // Get the symbols for the current beat in the pattern
+  let symbols = pattern[beat];
+
+  // If there's an 'x' in there, play the kick
+  if (symbols.includes('x')) {
+    kick.play();
+  }
+  // If there's an 'o' in there, play the snare
+  if (symbols.includes('o')) {
+    snare.play();
+  }
+  // If there's an '*' in there, play the hihat
+  if (symbols.includes('*')) {
+    hihat.play();
+  }
+  // Advance the pattern by a beat
+  beat = (beat + 1) % pattern.length;
+}
+
+// draw()
+//
+// Nothing right now. But imagine the possibilities!
+
+function draw() {}
