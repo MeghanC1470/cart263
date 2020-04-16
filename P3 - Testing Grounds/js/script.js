c661680b30c117344c18d2aaa7b694588bcbc829
Meghan <MeghanC2000@outlook.com>
2020-04-16 13:43:19 -0400

    P3: Added Range Slider and Added Testing Channel

---
+++ P3 - Testing Grounds/js/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/*****************
+
+Title
+Meghan Cullen
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+
+******************/
